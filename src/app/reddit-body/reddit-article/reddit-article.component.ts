6b4e47bb70d42b9ada681451336a2fb9444d6044
unknown <jainayrogeorge@outlook.com>
2017-10-21 08:58:11 -0400

    added some styling to the whole project,changed from css to sccs it is way better

--- src/app/reddit-body/reddit-article/reddit-article.component.ts
+++ src/app/reddit-body/reddit-article/reddit-article.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-reddit-article',
   templateUrl: './reddit-article.component.html',
-  styleUrls: ['./reddit-article.component.css']
+  styleUrls: ['./reddit-article.component.scss']
 })
 export class RedditArticleComponent implements OnInit {
 
--- src/app/reddit-body/reddit-body.component.ts
+++ src/app/reddit-body/reddit-body.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-reddit-body',
   templateUrl: './reddit-body.component.html',
-  styleUrls: ['./reddit-body.component.css']
+  styleUrls: ['./reddit-body.component.scss']
 })
 export class RedditBodyComponent implements OnInit {
 
