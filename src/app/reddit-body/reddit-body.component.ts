45554d69691179567b77b812d8f74b34bc812743
unknown <jainayrogeorge@outlook.com>
2017-10-22 12:26:18 -0400

    finished styling the form component, working on the article component

--- src/app/reddit-body/reddit-body.component.ts
+++ src/app/reddit-body/reddit-body.component.ts
@@ -12,4 +12,9 @@ export class RedditBodyComponent implements OnInit {
   ngOnInit() {
   }
 
+  addArticle(title: HTMLInputElement, link: HTMLInputElement) : boolean {
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    return false;
+  }
+
 }
